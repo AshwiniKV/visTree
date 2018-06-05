18b89d5f1351d9b75185bf34a6ea12d0b4ced506
Ashwini <a.venkatasubramaniam.1@research.gla.ac.uk>
2018-06-05 07:30:14 +0100

    Add the blsdata dataset

---
+++ R/blsdata.R
@@ -0,0 +1,46 @@
+blsdata<-NULL
+
+#' Box Lunch Study - Baseline dataset
+#' 
+#'The variables are as follows:
+#'
+#' \itemize{
+#'   \item trt. Treatment
+#'   \item sex. Sex
+#'   \item bmi0. BMI
+#'   \item snackkcal0. Snacking kilo calories 
+#'   \item srvgfv0. Serving size of fruits and vegetables
+#'   \item srvgssb0. Serving size of beverages
+#'   \item kcal24h0. 
+#'   \item edeq01. 
+#'   \item edeq02.
+#'   \item edeq13.
+#'   \item edeq14.
+#'   \item edeq15.
+#'   \item edeq22.
+#'   \item edeq23.
+#'   \item edeq25.
+#'   \item edeq26. 
+#'   \item cdrsbody0. Body image
+#'   \item weighfreq0. Weighing frequency
+#'   \item freqff0. Fast food frequency
+#'   \item age. Age
+#'   \item tfactor1.
+#'   \item tfactor2.
+#'   \item tfactor3.
+#'   \item mlhfbias0.
+#'   \item fwahfbias0.
+#'   \item rrvfood. Relative reinforcement of food
+#'   
+#'   
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name blsdata
+#' @usage data(blsdata)
+#' @format A data frame with 226 rows and 26 variables
+#' @examples
+#' data(blsdata)
+#' 
+"blsdata"
